f99cbcee9566867eddaf9bd8cc3f473e0d84ccc9
Joud awad <joud@epicdesignlabs.com>
2021-08-01 14:20:39 +0300

    finishing the project

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -1,30 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
 import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
       initialEntries: [initialPath],
     });
-  if (onNavigate) history.listen(onNavigate);
-  ReactDOM.render(<App history={history} />, el);
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-      if (pathname !== nextPathname) history.push(nextPathname);
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
     },
   };
 };
+
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
-  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
+  const devRoot = document.querySelector("#_auth-dev-root");
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
 }
+
 // We are running through container
 // and we should export the mount function
 export { mount };
